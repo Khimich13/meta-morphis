7f80e473a9942c1ac0c04bb6ced8a57d7371d154
Khimich13 <dober.man616@gmail.com>
2026-04-29 22:21:20 -0400

    Added fetch_cards file to get the detailed cards data by providing a list of card names from scryfall.com api; refactored goldfish_staple_scraper to get only names from the goldfish staples page
    Follow-up: added timeout parameter to the html request in goldfish_staple_scraper to avoid hanging; created todo comments in fetch_cards

---
+++ scripts/fetch_cards.py
@@ -0,0 +1,18 @@
+import requests
+from goldfish_staple_scraper import scrape_staple_names
+
+def fetch_many(names):
+    url = "https://api.scryfall.com/cards/collection"
+    identifiers = [{"name": n} for n in names]
+
+    r = requests.post(url, json={"identifiers": identifiers})
+    r.raise_for_status()
+    cards = r.json()["data"]
+    
+    for card in cards:
+        print(card["name"], card["mana_cost"], card["type_line"], card["oracle_text"])
+
+fetch_many(scrape_staple_names())
+
+# TODO: handle 75 cards limit per request
+# TODO: remove all print method
