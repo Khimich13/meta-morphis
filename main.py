239b4517b4f2be962f484b9e1ca3cf0e08875912
Khimich13 <dober.man616@gmail.com>
2026-07-21 23:09:42 -0400

    Add context manager for safe db operations

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ from meta_morphis.fetch_from_scryfall import fetch_cards
 from meta_morphis.generate_meta import get_meta_cards
 from meta_morphis.formats import FORMATS
 
-CONN = sqlite3.connect("cards.db")
-
 def choose_format() -> str:
     while True:
         print("Print the name of the format:\n")
@@ -26,27 +24,26 @@ def choose_format() -> str:
 
 
 def main():
-    init_db(CONN)
+    with sqlite3.connect("cards.db") as conn:
+        init_db(conn)
 
-    format = choose_format()
-    print(f"You chose: {format}\n")
+        format = choose_format()
+        print(f"You chose: {format}\n")
 
-    meta = get_meta_cards(CONN, format)
-    cards = fetch_cards(CONN, meta)
-    for card in cards:
-        name = card["name"]
-        type_line = card["type_line"]
-        mana_value = ""
+        meta = get_meta_cards(conn, format)
+        cards = fetch_cards(conn, meta)
+        for card in cards:
+            name = card["name"]
+            type_line = card["type_line"]
+            mana_value = ""
 
-        if "mana_cost" in card:
-            mana_value = card["mana_cost"]
-        # covers double-faced and other differently structured cards
-        elif "card_faces" in card:
-            mana_value = card["card_faces"][0].get("mana_cost", "")
-            
-        print(name, mana_value, type_line)
-
-    CONN.close()
+            if "mana_cost" in card:
+                mana_value = card["mana_cost"]
+            # covers double-faced and other differently structured cards
+            elif "card_faces" in card:
+                mana_value = card["card_faces"][0].get("mana_cost", "")
+                
+            print(name, mana_value, type_line)
 
 if __name__ == "__main__":
     main()
