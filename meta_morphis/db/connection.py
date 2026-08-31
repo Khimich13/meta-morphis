01614c3d5fa5bbd36ea879931295821f2fd2f410
Khimich13 <dober.man616@gmail.com>
2026-08-31 00:03:23 -0400

    Ensure that data folder exists to safely create db with get_connection()

--- meta_morphis/db/connection.py
+++ meta_morphis/db/connection.py
@@ -1,7 +1,10 @@
 import sqlite3
+from pathlib import Path
 
 import config
 
 
 def get_connection() -> sqlite3.Connection:
+    db_path = Path(config.DB_PATH)
+    db_path.parent.mkdir(parents=True, exist_ok=True)
     return sqlite3.connect(config.DB_PATH)
